09c170cc2207795927a200ba77c7e440d9ebad03
Gregor Hammerschmitt Andrade <hammerschmittgregor2@gmail.com>
2021-05-15 14:45:25 -0300

    feature: remobing id from userModel

--- src/data/models/user.ts
+++ src/data/models/user.ts
@@ -1,5 +1,3 @@
 import { User } from '@/domain/entities';
 
-// Serve para a camada de infraestrutura não ter conhecimento do domínio nas interfaces.
-// Se, por algum motivo a resposta do repositorie ficar diferente da entidade no domínio, podemos alterar aqui.
-export type UserModel = User;
+export type UserModel = Pick<User, 'email' | 'password' | 'name'>;
