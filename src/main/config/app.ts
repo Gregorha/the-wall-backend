4ead193f754fbd65fe466b3e102ea4bf66fd9796
Gregor Hammerschmitt Andrade <hammerschmittgregor2@gmail.com>
2021-05-16 19:19:45 -0300

    feature: cors config

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import { setupRoutes } from './routes';
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 setupRoutes(app)
 export default app;
